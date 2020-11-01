0ab1e62dbd3339fc4f991318f422600be2bfc408
Mateus <mvmsantana@tjba.jus.br>
2020-11-01 17:15:20 -0300

    form cadastro

--- front-react/src/views/CadastrarProduto.js
+++ front-react/src/views/CadastrarProduto.js
@@ -1,14 +1,43 @@
+import { Button, Card, CardContent, Container, Grid, TextField } from '@material-ui/core';
 import React from 'react';
 import Header from '../components/Header/Header';
+import './CadastrarProduto.css';
 
 export default class CadastrarProduto extends React.Component {
-
 
   render() {
     return (
       <div>
         <Header/>
-        <h2>Cadastro</h2>
+          <Container>
+            <Card>
+              <CardContent>
+                <div class="form-cadastro">
+                  <form> 
+                    <Grid  container spacing={1}>
+                      <Grid item xs={6}>
+                        <TextField id="nomeProduto" label="Nome do Produto"/>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <div></div>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <TextField id="nomeProduto" label="Valor do Produto"/>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <input type="file" class="inputFile" />
+                      </Grid>
+                      <Grid item xs={6}>
+                        <Button variant="contained" color="secondary">
+                          Cadastrar
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </div>
+              </CardContent>
+            </Card>
+          </Container>
       </div>
     )
   }
